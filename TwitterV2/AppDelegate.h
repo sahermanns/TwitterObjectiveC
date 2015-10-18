21121c36c29c68c3bd05be29fb171a5b3f02a019
Sarah Hermanns <sahermanns@gmail.com>
2015-10-18 07:41:25 -0700

    Initial Commit

---
+++ TwitterV2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TwitterV2
+//
+//  Created by Sarah Hermanns on 10/18/15.
+//  Copyright © 2015 SASH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TwitterV2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TwitterV2
+//
+//  Created by Sarah Hermanns on 10/18/15.
+//  Copyright © 2015 SASH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
