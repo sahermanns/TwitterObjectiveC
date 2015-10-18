70b115d2233807275254e3ec4852d89c493f2354
Sarah Hermanns <sahermanns@gmail.com>
2015-10-18 08:07:12 -0700

    initial commit

---
+++ TwitterV2/Tweet.h
@@ -0,0 +1,14 @@
+//
+//  Tweet.h
+//  TwitterV2
+//
+//  Created by Sarah Hermanns on 10/18/15.
+//  Copyright © 2015 SASH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface Tweet : NSObject
+
+@end
---
+++ TwitterV2/Tweet.m
@@ -0,0 +1,13 @@
+//
+//  Tweet.m
+//  TwitterV2
+//
+//  Created by Sarah Hermanns on 10/18/15.
+//  Copyright © 2015 SASH. All rights reserved.
+//
+
+#import "Tweet.h"
+
+@implementation Tweet
+
+@end
